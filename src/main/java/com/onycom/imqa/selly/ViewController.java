54982558110da90395dceb4dbc5bf308c00bae3f
sscheol <sucheol@onycom.com>
2024-02-19 13:26:31 +0900

    TestSelly.jar 삽입

--- src/main/java/com/onycom/imqa/selly/ViewController.java
+++ src/main/java/com/onycom/imqa/selly/ViewController.java
@@ -1,5 +1,6 @@
 package com.onycom.imqa.selly;
 
+import imqa.dashboard.runner.RunnerConsole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
